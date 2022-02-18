ab19cdf225fe571e69c2eac0798634d7f5e9dac0
Rosangela Rocha <ro.rochatur@gmail.com>
2022-02-18 10:58:58 -0300

    inclusao array

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 let paises = [];
+paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa'
+);
+console.table(paises);
