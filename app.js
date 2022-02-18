725d48bfd7271e309baecb8bca67a45b50e502c5
Rosangela Rocha <ro.rochatur@gmail.com>
2022-02-18 15:53:18 -0300

    alteração da lista

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let paises = [];
 paises.push('Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa'
 );
-console.table(paises);
+let i = paises
+for (let i = 0; i < paises.length; i++);
+console.log(i);
+
